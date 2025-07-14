870322ef499124254199ae0efffdb3c98f7a2837
whals <whalsdud0131@gmail.cm>
2025-07-14 09:29:19 +0900

    first commit

---
+++ src/test/java/com/my131/movie_review/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my131.movie_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
